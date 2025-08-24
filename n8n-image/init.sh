aa0ab3ef72742ff3016579c73d92733347cb640d
therealgenish <yoavg111@gmail.com>
2025-08-25 00:33:15 +0300

    changed enrtypoint for lambda compatible

--- n8n-image/init.sh
+++ n8n-image/init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 npm exec n8n import:workflow -- --input=/tmp/echo.json
 npm exec n8n update:workflow -- --id=echo --active=true
+
+# Start the n8n server in the background
 npm exec n8n start &
+
+# Give n8n a few seconds to start up
 sleep 10
-exec /lambda-entrypoint.sh "$@"
+
+# Now, we start the Lambda Runtime Interface Client (RIC)
+# This is the crucial step that hands over control to the Lambda runtime
+exec /usr/bin/aws-lambda-rie /var/task/lambda_handler.handler
