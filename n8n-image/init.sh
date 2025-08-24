232e65b43792519eec5728f4dfb225fc161dfdc5
therealgenish <yoavg111@gmail.com>
2025-08-25 01:46:42 +0300

    yet another minor fixed that causing major bug

--- n8n-image/init.sh
+++ n8n-image/init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 npm exec n8n import:workflow -- --input=/opt/echo.json
 npm exec n8n update:workflow -- --id=echo --active=true
-nohup npm exec n8n start > /tmp/n8n.log 2>&1 &
-sleep 15
+nohup npm exec n8n start &
 exec /lambda-entrypoint.sh "$@"
