64d94c48286c6b95620b5bfcac8b04604983e961
therealgenish <yoavg111@gmail.com>
2025-08-25 01:45:02 +0300

    add logs and fix typos yet againgit add .

--- n8n-image/init.sh
+++ n8n-image/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm exec n8n import:workflow -- --input=/opt/echo.json
 npm exec n8n update:workflow -- --id=echo --active=true
-npm exec n8n start &
-sleep 10
+nohup npm exec n8n start > /tmp/n8n.log 2>&1 &
+sleep 15
 exec /lambda-entrypoint.sh "$@"
