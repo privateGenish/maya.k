f5330f7101d8d6e311ef0fba10e944e1122db454
therealgenish <yoavg111@gmail.com>
2025-08-25 01:32:58 +0300

    another typo

--- n8n-image/init.sh
+++ n8n-image/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-npm exec n8n import:workflow -- --input=/tmp/echo.json
+npm exec n8n import:workflow -- --input=/opt/echo.json
 npm exec n8n update:workflow -- --id=echo --active=true
 npm exec n8n start &
 sleep 10
