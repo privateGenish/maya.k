8ddc8cb0fa9b68b50db3555452b0b566c80312f4
therealgenish <yoavg111@gmail.com>
2025-08-25 03:50:18 +0300

    adding logs and build cache

--- n8n-image/init.sh
+++ n8n-image/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 npm exec n8n import:workflow -- --input=/opt/echo.json
 npm exec n8n update:workflow -- --id=echo --active=true
-nohup npm exec n8n start &
+echo "Starting n8n..."
+nohup npm exec n8n start -- --host=0.0.0.0 --port=5678 > /tmp/n8n.log 2>&1 &
+echo "n8n started in background, logs at /tmp/n8n.log"
 exec /lambda-entrypoint.sh "$@"
